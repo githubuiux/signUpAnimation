fec85d5c146ac6e0c10005aa89c313ed4cc52f50
githubuiux <uiuxista@gmail.com>
2023-06-18 23:05:49 +0300

    Initial Commit

---
+++ signUpAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  signUpAnimation
+//
+//  Created by Levent on 18.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ signUpAnimation/signUpAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  signUpAnimationApp.swift
+//  signUpAnimation
+//
+//  Created by Levent on 18.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct signUpAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
